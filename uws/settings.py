f56fc16f3dcb4528aa093b1ff2a369dc2bdb8544
Alan Love <alan@dynamitealley.com>
2012-04-11 09:56:34 -0700

    more staticfiles bs

--- uws/settings.py
+++ uws/settings.py
@@ -10,7 +10,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 # tells Pinax to serve media through the staticfiles app.
-SERVE_MEDIA = True
+SERVE_MEDIA = DEBUG
 
 # django-compressor is turned off by default due to deployment overhead for
 # most users. See <URL> for more information
