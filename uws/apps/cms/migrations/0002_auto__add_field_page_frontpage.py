67e7937f90c45e30f368c400d1d54738c7ee23a2
Alan Love <alan@dynamitealley.com>
2012-04-12 13:44:35 -0700

    added migrations

---
+++ uws/apps/cms/migrations/0002_auto__add_field_page_frontpage.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Page.frontpage'
+        db.add_column('cms_page', 'frontpage',
+                      self.gf('django.db.models.fields.BooleanField')(default=False, unique=True),
+                      keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'Page.frontpage'
+        db.delete_column('cms_page', 'frontpage')
+
+    models = {
+        'cms.grouping': {
+            'Meta': {'object_name': 'Grouping'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'position': ('django.db.models.fields.IntegerField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'cms.page': {
+            'Meta': {'object_name': 'Page'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'frontpage': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'unique': 'True'}),
+            'grouping': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Grouping']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'cms.show': {
+            'Meta': {'object_name': 'Show'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'dj': ('django.db.models.fields.CharField', [], {'max_length': '420'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'songfile': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'songfile_length': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'songfile_size': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'year': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['cms']
