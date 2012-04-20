bf1065bd28ba1a89544e97eb64b5becb78513c4b
Alan Love <alan@dynamitealley.com>
2012-04-19 20:27:30 -0700

    initial player code

--- uws/apps/cms/menu_context.py
+++ uws/apps/cms/menu_context.py
@@ -1,8 +1,15 @@
 from cms.models import Show, Grouping
+import requests
 
 
 def side_menu(request):
     shows = Show.objects.order_by('-date').all()
     groupings = Grouping.objects.order_by('position').all()
 
-    return {'shows': shows, 'groupings': groupings}
+    r = requests.get('http://ultrawizardsword.net:8000/status.xsl')
+    if r.text == "dj":
+        live = True
+    else:
+        live = False
+
+    return {'shows': shows, 'groupings': groupings, 'live': live}
