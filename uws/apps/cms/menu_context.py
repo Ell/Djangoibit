1c507fce227f8a18949851d70ee8508572fa65db
Alan Love <alan@dynamitealley.com>
2012-04-19 20:31:56 -0700

    player fix

--- uws/apps/cms/menu_context.py
+++ uws/apps/cms/menu_context.py
@@ -7,7 +7,7 @@ def side_menu(request):
     groupings = Grouping.objects.order_by('position').all()
 
     r = requests.get('http://ultrawizardsword.net:8000/status.xsl')
-    if r.text == "dj":
+    if r.text == '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n\ndj\n\n':
         live = True
     else:
         live = False
